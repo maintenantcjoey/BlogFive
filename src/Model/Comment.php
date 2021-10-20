dda783b1782040a59d4ffcc0162421624e5a545e
Jessica Legrand <42817093+maintenantcjoey@users.noreply.github.com>
2021-10-20 09:07:34 +0200

    Fix post page

---
+++ src/Model/Comment.php
@@ -0,0 +1,83 @@
+<?php
+
+
+namespace Blog\Model;
+
+
+class Comment
+{
+
+    private $id;
+
+    private $content;
+
+    private $author;
+
+    private $post;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPost()
+    {
+        return $this->post;
+    }
+
+    /**
+     * @param mixed $post
+     */
+    public function setPost($post)
+    {
+        $this->post = $post;
+    }
+
+
+}
---
+++ src/Model/Manager/CommentManager.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Blog\Model\Manager;
+
+
+class CommentManager extends AbstractManager
+{
+
+}
